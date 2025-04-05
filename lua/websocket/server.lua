0d21a616b19f75eb4ce31d9edb4f8063411dcf1d
leonardo-luz <leonardoluz.f02@gmail.com>
2025-04-05 15:14:52 -0300

    fix: admin lines buffer update

--- lua/websocket/server.lua
+++ lua/websocket/server.lua
@@ -28,6 +28,25 @@ M.start_ws_server = function(port)
   run_goolang_func('startWsServer', vim.api.nvim_buf_get_lines(bufnum, 0, -1, false), port)
   vim.defer_fn(function()
     run_goolang_func('startWsClient', bufnum, "ws://localhost:" .. port .. "/ws")
+
+    local set_var = vim.api.nvim_buf_set_var
+    local get_var = vim.api.nvim_buf_get_var
+    local buf = vim.api.nvim_get_current_buf()
+    set_var(buf, "is_ws_update", false)
+
+    vim.api.nvim_create_autocmd("TextChangedI", {
+      callback = function()
+        if get_var(buf, "is_ws_update") then
+          set_var(buf, "is_ws_update", false)
+          return
+        end
+
+        local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false);
+        local line = table.concat(lines, "Ef232wefeEFAwdEFF")
+
+        run_goolang_func('writeWsClient', 0, -1, line, "ws://localhost:" .. port .. "/ws")
+      end
+    })
   end, 100)
 end
 
